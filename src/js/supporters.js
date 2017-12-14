9d6a70154085c0c2485caae4dd23515230280dbc
G01\hideki.nakazawa <hideki.nakazawa@fujitsu.com>
2017-12-14 15:13:53 +0900

    Merge branch 'master' of https://github.com/pj-pss/community-supporter-app

---
+++ src/js/supporters.js
@@ -0,0 +1,26 @@
+// function ID currently displayed
+var nowViewFunction = "proviedInfoList";
+// switch the screen to be displayed
+function view(functionId) {
+  $("#" + nowViewFunction).addClass('hidden');
+  $("#" + functionId).removeClass('hidden');
+  nowViewFunction = functionId;
+
+  $('.side-nav').find('.current').removeClass('current');
+  $('#menu_' + functionId).addClass('current');
+}
+
+// load html
+$(function() {
+  $("#proviedInfoList").load("proviedInfoList.html", null, function() {$("#editor").jqte();});
+  $("#operationHistory").load("operationHistory.html");
+  $("#disclosureInfotList").load("disclosureInfotList.html");
+  $("#tenantList").load("tenantList.html");
+});
+
+// load personal data and show modal window
+function openEditModal(name) {
+  $("#editModal").load("personalData.html #modal-edit_" + name, function(response){
+    $('#modal-edit_' + name).modal('show');
+  });
+}
